3cc43033ca07d235a810ed2cc82573636ba88f18
kumar6646 <sai.penusoft@gmail.com>
2025-04-25 11:44:07 +0530

    Assignment Operator Code

--- AssignmentOpe/Program.cs
+++ AssignmentOpe/Program.cs
@@ -8,8 +8,28 @@ namespace AssignmentOpe
 {
     internal class Program
     {
+        /*
+         * Assignment Operator is used assign the values
+         */
         static void Main(string[] args)
         {
+            int a = 100;
+            int b = a;
+            Console.WriteLine(a);
+            Console.WriteLine("value of b is"+b);
+            //a = a + 100;
+            a += 100;
+            Console.WriteLine("after adding"+a);
+            a-=100;
+            Console.WriteLine("after subtracting"+ a);
+            a *= 100;
+            Console.WriteLine("after multiplication" + a);
+            a /= 100;
+            Console.WriteLine("after division" + a);
+            a %= 100;
+            Console.WriteLine("after modulus" + a);
+
+
         }
     }
 }
